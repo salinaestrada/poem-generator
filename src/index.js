e4c308afb6eb8f17a81817863f77178bada47b3c
salinaestrada <salina.lenay@gmail.com>
2024-05-13 18:00:16 -0500

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: ["generated poem"],
+    autoStart: true,
+    delay: 1,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-topic");
+poemFormElement.addEventListener("submit", generatePoem);
