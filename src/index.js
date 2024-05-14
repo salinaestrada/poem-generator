a8be80e8d3ff258390b97ba5be1b7c87a5c97b2f
salinaestrada <salina.lenay@gmail.com>
2024-05-13 19:31:07 -0500

    Improved the UX

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ function generatePoem(event) {
     "You are an chicana poet. You write mostly in English but include Spanish words as well. Your job is to write an 4 line ode using the user instructions from the prompt. separate each line with a <br />. Do not include a title. Sign the code with 'Written using SheCodes AI' inside a <small> element at the end of the poem.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class = "blink">Generating a poem about ${instructionsInput.value}...</div>`;
+
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
 
